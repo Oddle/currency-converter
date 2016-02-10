44f3c46a94e525a16b63b93d592b47e9a67b161e
joonhui <joonhui@gmail.com>
2016-02-10 14:27:35 +0800

    commited CSS, HTML and JS

---
+++ oddle.js
@@ -0,0 +1,19 @@
+(function() {
+  console.log('hi');
+
+  //var
+  //string
+  //int
+  window.sumOfTwoNumbers(1, 3);
+
+  //array
+
+  //loop
+
+
+})()
+
+
+function sumOfTwoNumbers(x, y) {
+  console.log(x + y);
+}
