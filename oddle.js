4adb750078562c1a0693f46d109fcf9646ba7a70
joonhui <joonhui@gmail.com>
2016-02-24 11:00:13 +0800

    added Javascript
    added Javascript

--- oddle.js
+++ oddle.js
@@ -1,19 +1,58 @@
-(function() {
-  console.log('hi');
-
-  //var
-  //string
-  //int
-  window.sumOfTwoNumbers(1, 3);
-
-  //array
-
-  //loop
-
+(function(){
+  var results = sumOfFourVariables(4, 5, 6, 7);
+  console.log(results);
 
 })()
 
-
-function sumOfTwoNumbers(x, y) {
-  console.log(x + y);
+function sumOfTwoVariables(one, two){
+  var total = one + two;
+  return total;
 }
+
+function sumOfFourVariables(a, b, c, d) {
+  var x = sumOfTwoVariables(a,b);
+  var y = sumOfTwoVariables(c,d);
+  var z = sumOfTwoVariables(x,y);
+  return z;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// (function() {
+//   console.log('hi');
+//
+//   //var
+//   //string
+//   //int
+//   window.sumOfTwoNumbers(1, 3);
+//
+//   //array
+//
+//   //loop
+//
+//
+// })()
+//
+//
+// function sumOfTwoNumbers(x, y) {
+//   console.log(x + y);
+// }
